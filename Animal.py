e7ded6bb6ece50dccbc6eccfa3095d3540fbbf89
strup <strupkanorbert@gmail.com>
2022-07-06 13:10:38 +0200

    setting up the variables from the input file.

---
+++ Animal.py
@@ -0,0 +1,8 @@
+
+class Pet(object):
+    def __init__(self, animaltype, animalname, animalhealth ):
+        self.animaltype = animaltype
+        self.animalname = animalname
+        self.animalhealth = animalhealth
+
+
--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+from Animal import Pet
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+allpets = []
+petattributenames = ['Type', 'Name', 'Health']
+
+with open("TestFile.txt") as f:
+    numberOfPets = f.readline()
+    if numberOfPets != 0:
+        for x in range(0, int(numberOfPets)):
+            getlinefromfile = f.readline().split()
+            d = dict(zip(petattributenames, getlinefromfile))
+            allpets.append(d)
+    moodchain = f.readline()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
